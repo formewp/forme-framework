7476636a3336dbdb1bf88e2a5e9c89e7e095ca60
Moussa Clarke <moussaclarke@gmail.com>
2022-06-21 21:06:39 +0100

    fix: collect typehint

--- src/Models/Post.php
+++ src/Models/Post.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 namespace Forme\Framework\Models;
 
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Collection;
 
 /**
  * @property int                        $ID
