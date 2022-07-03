4b30619a8081129df05bfbb6cbca287487a1957d
Moussa Clarke <moussaclarke@gmail.com>
2022-07-03 08:32:54 +0100

    test: rename test groupings

--- tests/Pest.php
+++ tests/Pest.php
@@ -4,10 +4,10 @@ use Yoast\WPTestUtils\BrainMonkey\TestCase;
 
 define('FORME_PRIVATE_ROOT', __DIR__ . '/../');
 
-uses(TestCase::class)->in('Integration');
+uses(TestCase::class)->in('WordPress');
 
 // make sure wordpress is re-initialised before each test
 uses()->beforeEach(function () {
     global $wp_filter;
     $wp_filter = [];
-})->in('Integration');
+})->in('WordPress');
