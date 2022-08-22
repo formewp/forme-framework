e5d51c81336b26ffc7650f294ed8e93f7cdada94
Moussa Clarke <moussaclarke@gmail.com>
2022-08-22 20:29:23 +0100

    feat: magic index view resolve

--- src/View/PlatesView.php
+++ src/View/PlatesView.php
@@ -11,14 +11,21 @@ class PlatesView implements ViewInterface
 
     private \League\Plates\Engine $view;
 
+    private const RELATIVE_VIEW_DIR = '/../../views';
+
     public function __construct()
     {
-        $this->view = new Engine($this->getDir() . '/../../views', 'plate.php');
+        $this->view = Engine::create($this->getDir() . self::RELATIVE_VIEW_DIR, 'plate.php');
     }
 
     public function render(string $template, array $context = []): string
     {
         $template = str_replace('.', '/', $template);
+        $viewDir  = $this->getDir() . self::RELATIVE_VIEW_DIR . '/';
+
+        if (!file_exists($viewDir . $template . 'plate.php') && file_exists($viewDir . $template . '/index.plate.php')) {
+            $template .= '/index';
+        }
 
         return $this->view->render($template, $context);
     }
