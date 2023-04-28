ce684c76b82fdceb611d22995d16ec1daa6aa5b8
Moussa Clarke <moussaclarke@gmail.com>
2023-04-28 21:05:55 +0100

    feat: meta relations and typing

--- src/Models/PostMeta.php
+++ src/Models/PostMeta.php
@@ -4,7 +4,15 @@ declare(strict_types=1);
 namespace Forme\Framework\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @property int    $meta_id
+ * @property string $meta_key
+ * @property string $meta_value
+ * @property int    $post_id
+ * @property Post   $post
+ */
 class PostMeta extends Model
 {
     protected $primaryKey = 'meta_id';
@@ -12,4 +20,9 @@ class PostMeta extends Model
     public $timestamps = false;
 
     protected $table = 'postmeta';
+
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(Post::class, 'post_id', 'ID');
+    }
 }
--- src/Models/UserMeta.php
+++ src/Models/UserMeta.php
@@ -4,7 +4,15 @@ declare(strict_types=1);
 namespace Forme\Framework\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @property int    $umeta_id
+ * @property int    $user_id
+ * @property User   $user
+ * @property string $meta_key
+ * @property string $meta_value
+ */
 class UserMeta extends Model
 {
     protected $primaryKey = 'umeta_id';
@@ -12,4 +20,9 @@ class UserMeta extends Model
     public $timestamps = false;
 
     protected $table = 'usermeta';
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id', 'ID');
+    }
 }
