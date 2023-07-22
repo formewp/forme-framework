2b428d497616847eed2fdc852430385db21982cc
Moussa Clarke <moussaclarke@gmail.com>
2023-07-22 15:17:06 +0200

    chore: include altorouter directly due to upstream global namespace

--- src/helpers.php
+++ src/helpers.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Forme;
 
-use AltoRouter;
+use Forme\Framework\Router\AltoRouter;
 use DI\ContainerBuilder;
 use DI\FactoryInterface;
 use Forme\Framework\Http\ServerRequest;
